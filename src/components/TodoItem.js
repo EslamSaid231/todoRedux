c6821a72561b87d1e3f9956f6c68be040af9b082
EslamSaid231 <djdjksks@gmail.com>
2023-03-06 02:43:14 +0200

    todoRedux

---
+++ src/components/TodoItem.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import {
+  DeleteTodoAsync,
+  getTodosAsync,
+  TodoActions,
+  toggleCompleteAsync,
+} from "../Redux/todoSlice";
+
+const TodoItem = ({ id, title, completed }) => {
+  const dispatch = useDispatch();
+  const handleComplete = () => {
+    dispatch(toggleCompleteAsync({ id: id, completed: !completed }));
+  };
+
+  const handleDelete = () => {
+    dispatch(DeleteTodoAsync({ id }));
+  };
+
+  return (
+    <li className={`list-group-item ${completed && "list-group-item-success"}`}>
+      <div className="d-flex justify-content-between">
+        <span className="d-flex align-items-center">
+          <input
+            type="checkbox"
+            className="mr-3"
+            checked={completed}
+            onChange={handleComplete}
+          ></input>
+          {title}
+        </span>
+        <button className="btn btn-danger" onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoItem;
---
+++ src/components/TodoList.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  DeleteTodoAsync,
+  getTodosAsync,
+  TodoActions,
+} from "../Redux/todoSlice";
+import TodoItem from "./TodoItem";
+
+const TodoList = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+
+  useEffect(() => {
+    dispatch(getTodosAsync());
+  }, [dispatch]);
+  return (
+    <ul className="list-group">
+      {todos.map((todo) => (
+        <TodoItem id={todo.id} title={todo.title} completed={todo.completed} />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
